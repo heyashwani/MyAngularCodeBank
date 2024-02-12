2567e84f59a14c897d5e5a82281ffff3d7372259
open@4425 <imashwani006@gmail.com>
2024-02-12 18:38:09 +0530

    test

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -21,13 +21,10 @@ export class CountdownComponent implements OnInit {
     setInterval(()=>{
       this.countDownTimer()
     },1000)
-
-    
-    this.countDownTimer()
   }
 
   countDownTimer(){
-    var futureDate = new Date("Oct 21 2023 15:30:00").getTime();
+    var futureDate = new Date("March 18 2024 15:30:00").getTime();
     var today = new Date().getTime();
     var distance = futureDate - today;
     this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -35,6 +32,17 @@ export class CountdownComponent implements OnInit {
     this.mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
     this.secs = Math.floor((distance % (1000 * 60)) / (1000))
     console.log(this.days)
+  }
+
+  convertDateTime(){
+    const indiaDateTime = new Date();
+  
+    const swedenDatetime = indiaDateTime.toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' });
+  
+    console.log("India==>> ",indiaDateTime)
+    console.log("India==>> ",swedenDatetime)
+
+    /// to confirm check on google, write "sweden current date time"
   }
 
 }
